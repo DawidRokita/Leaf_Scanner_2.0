17f4be769784e3be328f1b9f196b6a5e30b6d1f8
DawidRokita <dawidrokita11@gmail.com>
2026-01-16 17:36:56 +0100

    commit test

--- app/src/main/java/com/example/leaf_scanner_test_2/CameraActivity.kt
+++ app/src/main/java/com/example/leaf_scanner_test_2/CameraActivity.kt
@@ -21,7 +21,7 @@ class CameraActivity : AppCompatActivity() {
     ) { granted ->
         if (granted) startCamera()
     }
-
+    //test coment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
